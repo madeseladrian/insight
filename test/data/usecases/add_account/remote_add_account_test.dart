a5063cd699c5040ae21aec73f53b0a2e94c625a4
Mades Eladrian <mades.eladrian@gmail.com>
2023-01-11 15:10:28 -0300

    ensure RemoteAddAccount throws UnexpectedError if HttpClient returns 500

--- test/data/usecases/add_account/remote_add_account_test.dart
+++ test/data/usecases/add_account/remote_add_account_test.dart
@@ -67,4 +67,10 @@ void main() {
     final future = sut.add(params: params);
     expect(future, throwsA(DomainError.unexpected));
   });
+
+  test('8 - Should throw UnexpectedError if HttpClient returns 500', () async {
+    httpClient.mockRequestError(HttpError.serverError);
+    final future = sut.add(params: params);
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
