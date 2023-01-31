154a221a8f312675ef3a3506f1df7fb0728807c5
Mades Eladrian <mades.eladrian@gmail.com>
2023-01-31 20:01:20 -0300

    ensure InitialPage calls goToCamer on form submit

--- test/ui/pages/initial_page_test.dart
+++ test/ui/pages/initial_page_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:insight/ui/pages/pages.dart';
+import 'package:mocktail/mocktail.dart';
 
 import '../mocks/mocks.dart';
 
@@ -20,5 +21,16 @@ void main() {
   
     final button = tester.widget<ElevatedButton>(find.byType(ElevatedButton));
     expect(button.onPressed, isNotNull);
+  });
+
+  testWidgets('2 - Should call goToCamera on form submit', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    final button = find.byType(ElevatedButton);
+    await tester.ensureVisible(button);
+    await tester.tap(button);
+    await tester.pump();
+
+    verify(() => presenter.goToCamera()).called(1);
   });
 }
